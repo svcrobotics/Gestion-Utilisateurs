f60d88f6741a7f49668b544c02c5a9345d0e80f7
Victor Perez <svcrobotics@gmail.com>
2019-02-23 09:13:50 +0100

    Simple sauvegarde, codage en cours

--- user.py
+++ user.py
@@ -13,37 +13,40 @@ import sys
 import datetime
 
 # Tout d'abord on vérifie que le script a bien été exécuté 
-# en tant que root (la commande getuid retourne l'identifiant 
-# numérique de l'utilisateur)
+# en tant que root (la fonction geteuid() retourne l'identifiant 
+# numérique de l'utilisateur) 0 dans le cas de root.
 
-if os.getuid == 0:
-    print("Cette commande doit être lancée en tant que root.")
+if os.geteuid() == 0:
+    print("Vous avez bien lancer le script en tant que root. => pass1")
 else:
-    print("Vous avez bien lancer la commande en tant que root => pass1")
+    print("Cette commande doit être lancée en tant que root!")
+    sys.exit(0)
 
 # Ensuite, on teste si le chemin du fichier CSV a été fourni 
 # comme premier argument à la commande (si ce n'est pas le cas,
 # on quitte le programme en rappelant que le fichier est 
 # nécessaire):
 
-if len(sys.argv) != 1:
+if len(sys.argv) > 1:
     fichier_csv = sys.argv[1]
-    print("Vous avez bien passez un fichier en paramètre => pass2")
+    print("Vous avez bien passez un fichier en paramètre. => pass2")
 else:
     print("Ou est le fichier .csv ?")
-    sys.exit()
+    sys.exit(0)
 
 # Enfin, on vérifie que le fichier en question existe 
-# (on stoppe le script s'il n'existe pas):
+# (on stoppe le script s'il n'existe pas) et que l'extension du fichier
+# est bien .csv pour evité d'ouvrir un autre type de fichier:
 
 exists = os.path.exists(fichier_csv)
+# basename permet d'obtenir le nom d'un fichier et son extension à partir d'un chemin d'accès
 basename(fichier_csv)
 fileName, fileExtension = os.path.splitext(fichier_csv)
 if exists and fileExtension == '.csv':
     print("Le fichier .csv existe => pass3")
 else:
-    print("Le fichier .csv n'existe pas!")
-    sys.exit()
+    print("Le chemin du fichier .csv est incorrect ou l'extension de celui-ci n'est pas .csv!")
+    sys.exit(0)
 
 ##################################
 # Définition locale de fonctions #
@@ -61,28 +64,28 @@ def lire_lignes():
     for line in f.readlines():
         
         tab = line.split()
+        password = tab[2]
         nom = formatage(tab[1])
         prenom = formatage(tab[0])
         groupe = formatage(tab[3])
-
         user = nom + "_" + prenom
-        creer_groupe(user, groupe)
+        #creer_groupe(user, groupe, password)
 
         i = i + 1
     f.close()
 #################################################
 
 def formatage(valeur):
-    val = valeur.lower().replace("é", "e").replace(".", "")
+    val = valeur.lower().replace("é", "e").replace(".", "").rstrip('\n\r')
     return val
 
 ###################################################
 # 
-def creer_groupe(user, groupe):
-    
+'''
+def creer_groupe(user, groupe, password):
+
     path = "/home"
     path2 = os.path.join(path, groupe)
-    global path3 
     path3 = os.path.join(path2, user)
 
     if os.path.exists(path2):
@@ -90,19 +93,21 @@ def creer_groupe(user, groupe):
 
         if os.path.exists(path3):
             print(path3, " exist!", " pass7")
-            if groupe == "-":
-                print(user)
-                #archiver_user(user)
         else:
             os.mkdir(path3)
             print(path3, "vient d'être crée!", " pass8")
     else:
-        if groupe != "-":
-            os.mkdir(path2)
-            os.mkdir(path3)
-            
-            print("Le groupe: ", groupe, " et l'utilisateur: ", user, 
-            " viennent d'être crées!" ," pass9")
+        
+        os.mkdir(path2)
+        os.system("groupadd " + groupe)
+        
+        creer_user(user, password, groupe)
+        print("Le groupe: ", groupe, " et l'utilisateur: ", user, " viennent d'être crées!" ," pass9")
+'''
+def creer_groupe(groupe):
+    path = "/home/" + groupe
+    os.mkdir(path)
+    os.system("groupadd " + groupe)
 
 ############################################################
 # La fonction suprimer_groupe essaie de supprimer le répertoire
@@ -110,8 +115,10 @@ def creer_groupe(user, groupe):
 # on peut alors le supprimer.
 
 def supprimer_groupe(groupe):
-    os.system("rm -R /home/" + groupe)
     
+    os.system("groupdel " + groupe)
+    os.system("rm -R /home/" + groupe + "/*")
+
 ###########################################################
 # Pour archiver un utilisateur, il faut que Lieu soit égal à -
 # dans le fichier .csv
@@ -131,16 +138,39 @@ def archiver_user(user):
     os.system("rm -R " + path3)
 
 
-def chercher_user(full_path):
-    
-    print("This file directory and name")
-    path, filename = os.path.split(full_path)
-    print(path + ' --> ' + filename + "\n")
-##########################################
+def chercher_user(user):
+    print(os.system("id -u " + user))
+    print(os.system("id -g " + user))
+    print(os.system("echo ~" + user))
 
+def creer_user(user, password, groupe):
+    # Le répertoire squelette (/etc/skel/) contient les fichiers et 
+    # répertoires qui seront copiés dans le répertoire 
+    # personnel de l’utilisateur au moment de sa création.
+    
+    os.system("useradd -d /home/" + groupe + "/" + user + " -s /bin/bash " + " -g " + groupe + " --password  $(mkpasswd -H md5 " + password + " ) " + user )
+    #os.mkdir("/home/" + groupe + "/" + user)
+    os.system("passwd -e " + user)
+    os.system("cp -r /etc/skel/* /home/" + groupe + "/" + user + "/")
+    print(os.system("ls -al /home/"+ groupe + "/" + user))
+
+def delete_user(user):
+    os.system("userdel -r -f " + user)
+
+ 
+##########################################
+print("####################################################")
 #lire_lignes()
-jm = "jmtlg_planteur"
-full_path = "/home/lancre/ciredutemps_esmeralda"
-chercher_user(full_path)
-groupe = "universite"
+print("####################################################")
+#chercher_user()
 #supprimer_groupe(groupe)   
+#print(os.system("cat /etc/group"))
+#print(os.system("cat /etc/shadow"))
+
+###############################""
+
+#creer_groupe("admin")
+creer_user("victor", "Victor@1972", "admin")
+#chercher_user("victor")
+#delete_user('victor')
+#supprimer_groupe("admin")
